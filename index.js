74ffa4767b9b1a4ec20247fa10f2c226e782e1ca
demetriotorgan <demetriotorgan@hotmail.com>
2024-10-16 14:58:49 -0300

    commit:cors4

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use((req,res,next)=>{
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Methods","GET,PUT,POST,DELETE")
     res.header("Access-Control-Allow-Headers", "Content-Type")
+    res.setHeader("Access-Control-Allow-Origin", "*");
     app.use(cors())
     next()
 });
