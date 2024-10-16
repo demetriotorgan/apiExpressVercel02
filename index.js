e5d12f96099e6c563c427469c0b8ecadfdfe5282
demetriotorgan <demetriotorgan@hotmail.com>
2024-10-16 15:48:54 -0300

    commit:cors9

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 //Cors
 app.use((req,res, next)=>{
-    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
     app.use(cors())
     next()
