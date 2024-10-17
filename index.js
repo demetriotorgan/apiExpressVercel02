12d48da7fe661dfcfe1fc27b7cb3794b8092884b
demetriotorgan <demetriotorgan@hotmail.com>
2024-10-17 12:18:59 -0300

    corsteste

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 //Cors
 app.use((req,res, next)=>{
     res.header('Access-Control-Allow-Origin', '*');
-    //res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
     next();    
   });
   app.use(cors({
